00b975d370646b5ea55f42f00492b05bb672757f
Nikola Sevo <nsevonikola@gmail.com>
2020-12-03 08:21:33 +0100

    Part1

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 "use strict";
 exports.__esModule = true;
 var data_1 = require("./data");
-var array = data_1.data.split('\n');
-var result = 0;
-array.forEach(function (x, i) {
-    array.forEach(function (y, j) {
-        array.forEach(function (z, k) {
-            var intx = parseInt(x);
-            var inty = parseInt(y);
-            var intz = parseInt(z);
-            if (i !== j &&
-                j !== k &&
-                ((intx + inty + intz) === 2020)) {
-                result = intx * inty * intz;
-            }
-        });
-    });
+var rows = data_1.data.split('\n');
+var validatad = rows
+    .map(function (x) {
+    var split = x.split(': ');
+    var rule = split[0];
+    var ruleSplit = rule.split(' ');
+    var password = split[1];
+    var minMaxString = ruleSplit[0]; // 1-2
+    var min = parseInt(minMaxString.split('-')[0]);
+    var max = parseInt(minMaxString.split('-')[1]);
+    var letter = ruleSplit[1]; // a
+    var regexp = new RegExp("[^" + letter + "]", 'g');
+    var letterOccurrance = password.replace(regexp, '').length;
+    /*console.dir({
+      password,
+      min,
+      max,
+      letter,
+      regexp,
+      letterOccurrance
+    });*/
+    return (letterOccurrance <= max) && (letterOccurrance >= min);
 });
-console.log(result);
+console.log('valid:');
+console.log(validatad.filter(function (x) { return !!x; }).length);
+console.log('of:');
+console.log(rows.length);
