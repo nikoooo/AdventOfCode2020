93ccbe27e8f0d6b647f721bf75fde553d1b33981
Nikola Sevo <nikola.sevo@weknowit.nu>
2020-12-05 09:38:31 +0100

    day1b

--- day1/b/index.ts
+++ day1/b/index.ts
@@ -1,1 +1,24 @@
 import { data } from './data'
+
+const array = data.split('\n');
+
+let result = 0;
+
+array.forEach((x, i) => {
+  array.forEach((y, j) => {
+    array.forEach((z, k) => {
+    var intx = parseInt(x);
+    var inty = parseInt(y);
+    var intz = parseInt(z);
+      if (
+        i !== j &&
+        j !== k &&
+        ((intx + inty + intz) === 2020)
+      ) {
+        result = intx * inty * intz;
+      }
+    });
+  });
+})
+
+console.log(result);
